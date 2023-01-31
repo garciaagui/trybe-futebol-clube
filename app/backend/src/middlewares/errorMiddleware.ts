7bc49bcb40dc9b0204e855903e01b978de6be281
Guilherme Garcia <garciaguig@gmail.com>
2023-01-31 20:49:11 -0300

    :bulb: docs: Remove comentário do arquivo de errorMiddleware.ts

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -4,7 +4,6 @@ import HttpException from '../exceptions/HttpException';
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   const { status, message } = err as HttpException;
   res.status(status || 500).json({ message });
-  // res.status(status).json({ message });
 };
 
 export default errorMiddleware;
