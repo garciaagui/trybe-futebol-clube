73706d2def0f7e0ef69597aa2243420837088448
Guilherme Garcia <garciaguig@gmail.com>
2023-02-11 09:51:21 -0300

    :wrench: chore: Altera o tempo de expiração do token

--- app/backend/src/utils/generateToken.ts
+++ app/backend/src/utils/generateToken.ts
@@ -6,7 +6,7 @@ const SECRET_KEY:jwt.Secret = process.env.JWT_SECRET as string;
 export default (email: string): string => {
   const payload = { email };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '6h', algorithm: 'HS256' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h', algorithm: 'HS256' });
 
   return token;
 };
